188c9b4d080bef47d1b80bc0355160d5fee92541
Aleksey Evtifiev <evtifiev@gmail.com>
2019-09-06 23:41:47 +0300

    default Vue template

--- src/main.js
+++ src/main.js
@@ -1,12 +1,13 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './app'
+import router from '@router'
+import store from '@state/store'
+import '@components/_globals'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = process.env.NODE_ENV === 'production'
 
-new Vue({
+const app = new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount("#app");
+  render: (h) => h(App),
+}).$mount('#app')
---
+++ src/router/layouts/main.vue
@@ -0,0 +1,17 @@
+<script>
+import NavBar from '@components/nav-bar'
+export default {
+  components: { NavBar },
+}
+</script>
+
+<template>
+  <div :class="$style.container">
+    <NavBar />
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" module>
+@import '@design';
+</style>
